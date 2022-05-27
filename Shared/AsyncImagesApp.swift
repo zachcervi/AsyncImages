22fd812d2f9e3e6c3c84ed0af35d30a91da937f1
Zach Cervi <zachcervi@gmail.com>
2022-05-27 15:03:00 -0600

    Initial Commit

---
+++ Shared/AsyncImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImagesApp.swift
+//  Shared
+//
+//  Created by Zach Cervi on 5/27/22.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
